d5760d3f8e11b6e27ab563f53cc926167936d68e
Rolando Aldana <lBlackDev@users.noreply.github.com>
2026-09-21 23:51:05 -0300

    CI/CD : 15

--- pedidos360-frontend/src/environments/environment.ts
+++ pedidos360-frontend/src/environments/environment.ts
@@ -4,8 +4,8 @@ export const environment = {
     clientId: 'd4b53b0b-a9b3-42b4-b63d-71d24f58813b',
     tenantId: 'bb5324af-c266-41ed-b36c-a971641c7af2',
     authority: 'https://login.microsoftonline.com/bb5324af-c266-41ed-b36c-a971641c7af2',
-    redirectUri: 'http://98.84.170.100',
+    redirectUri: 'http://54.242.195.23',
     protectedResourceScopes: ['api://d4b53b0b-a9b3-42b4-b63d-71d24f58813b/OT.Create'],
   },
-  apiBaseUrl: 'http://98.84.170.100:8080',
+  apiBaseUrl: 'http://54.242.195.23:8080',
 };
